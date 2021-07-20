df6dc5f952b0d17455d5a3c86ae37f54bcf1d63a
RanDuan <aduan456@gmail.com>
2021-07-20 23:10:22 +0800

    添加定时器5

--- USER/main.c
+++ USER/main.c
@@ -21,6 +21,7 @@
 #include "sram.h"  
 #include "keyboard.h"
 #include "flash.h"
+#include "timer.h"
 
 void test_uart1(void);
 void test_uart2(void);
@@ -48,10 +49,40 @@ void test_keyboard(void);
 ///////////////////////////////
 void test_fatfs(void);
 void test_flash(void);
+void test_timer3(void);
+void test_timer5(void);
 
 int main(void)
 {	
-	test_flash();
+	test_timer5();
+}
+
+void test_timer5(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+	delay_init();  //初始化延时函数
+	led_init();				//初始化LED端口
+
+ 	TIM5_Int_Init(5000-1,8400-1);	//定时器时钟84M，分频系数8400，所以84M/8400=10Khz的计数频率，计数5000次为500ms     
+	while(1)
+	{
+		led_switch(LED0);//DS0翻转
+		delay_ms(200);//延时200ms
+	};
+}
+
+void test_timer3(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
+	delay_init();  //初始化延时函数
+	led_init();				//初始化LED端口
+
+ 	TIM3_Int_Init(5000-1,8400-1);	//定时器时钟84M，分频系数8400，所以84M/8400=10Khz的计数频率，计数5000次为500ms     
+	while(1)
+	{
+		led_switch(LED0);//DS0翻转
+		delay_ms(200);//延时200ms
+	};
 }
 
 
